02bb4b2fc117189ba23881e24d34d3bb2ab54671
Sachin Meena <82491840+sachin492002@users.noreply.github.com>
2023-03-24 21:45:07 +0530

    product and feeds

---
+++ backend/models/product.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 32,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 2000,
+  },
+  price: {
+    type: Number,
+    trim: true,
+    required: true,
+    maxlength: 32,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 32,
+  },
+  category: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 32,
+  },
+  shipping: {
+    type: Boolean,
+    required: false,
+    
+  },
+  seller:{
+    type: String,
+    required: false,
+  },
+  stock:{
+    type: Number,
+    required: true,
+  },
+  reviews:{
+    type: Number,
+    required:false,
+  },
+  stars:{
+    type: Number,
+    required:false,
+  }
+});
+module.exports = mongoose.model('Product', productSchema);
